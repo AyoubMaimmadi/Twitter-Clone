012f4aa0e98d1939bb67239af509d68c7e0d23a8
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-08-02 15:56:06 +0100

    new

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
+import Widgets from '../components/Widgets'
 
 const Home: NextPage = () => {
   return (
@@ -13,10 +15,8 @@ const Home: NextPage = () => {
 
       <main>
         <Sidebar />
-
-        {/* feed */}
-
-        {/* widgets  */}
+        <Feed />
+        <Widgets />
       </main>
     </div>
   )
